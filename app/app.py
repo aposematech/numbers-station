70bbd2ba55cf046a60a2d667e62773d84b2d551b
Jon Faviell <jon.faviell@gmail.com>
2022-08-29 00:41:42 -0500

    edit function

--- app/app.py
+++ app/app.py
@@ -39,7 +39,7 @@ def handler(event, context):
     one_time_pad = get_one_time_pad(alphabet, plain_text)
     cipher_code = get_cipher_code(alphabet, plain_text, one_time_pad)
     cipher_code_blocks = get_code_blocks(cipher_code, 5)
-    # post_tweet(cipher_code_blocks)
+    post_tweet(cipher_code_blocks)
     return { 
         "statusCode": 200,
         "headers": {
