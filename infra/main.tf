174e2f7af8336ce654bc2a5c51b6c864272813db
Jon Faviell <jon.faviell@gmail.com>
2022-08-29 19:04:32 -0500

    add comment

--- infra/main.tf
+++ infra/main.tf
@@ -121,6 +121,7 @@ resource "aws_lambda_function" "lambda_function" {
   package_type  = "Image"
   image_uri     = "${var.account_number}.dkr.ecr.${var.region}.amazonaws.com/${terraform.workspace}:latest"
   role          = aws_iam_role.lambda_role.arn
+  # TODO find a better way
   environment {
     variables = {
       CONSUMER_KEY        = var.twitter_consumer_key,
