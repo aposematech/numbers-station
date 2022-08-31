1b92e6de0b60d09806553542b6bcfb521ef0afb3
Jon Faviell <jon.faviell@gmail.com>
2022-08-31 00:57:32 -0500

    add qrcode

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@
 # https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
 # https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
 
+import qrcode # https://pypi.org/project/qrcode/
 import random
 import string
 import tweepy # https://docs.tweepy.org/en/stable/
@@ -30,18 +31,31 @@ def handler(event, context):
     one_time_pad = get_one_time_pad(alphabet, plain_text)
     cipher_code = get_cipher_code(alphabet, plain_text, one_time_pad)
     cipher_code_blocks = get_code_blocks(cipher_code, 5)
+    # get qrcode
+    cipher_qr_code_filename = "cipher_qr_code.png"
+    cipher_qr_code = qrcode.make(cipher_code_blocks)
+    cipher_qr_code.save(cipher_qr_code_filename)
     # get secrets
     twitter_consumer_key = parameters.get_secret("twitter_consumer_key")
     twitter_consumer_secret = parameters.get_secret("twitter_consumer_secret")
     twitter_access_token = parameters.get_secret("twitter_access_token")
     twitter_access_token_secret = parameters.get_secret("twitter_access_token_secret")
+    # get auth
+    auth = tweepy.OAuth1UserHandler(
+        twitter_consumer_key, 
+        twitter_consumer_secret, 
+        twitter_access_token, 
+        twitter_access_token_secret)
+    api = tweepy.API(auth)
+    # upload qrcode
+    qr_code = api.media_upload(cipher_qr_code_filename)
     # post code
     client = tweepy.Client(
         consumer_key=twitter_consumer_key, 
         consumer_secret=twitter_consumer_secret, 
         access_token=twitter_access_token, 
         access_token_secret=twitter_access_token_secret)
-    client.create_tweet(text=cipher_code_blocks)
+    client.create_tweet(text=cipher_code_blocks, media_ids=qr_code.media_id_string)
     return { 
         "statusCode": 200,
         "headers": {
