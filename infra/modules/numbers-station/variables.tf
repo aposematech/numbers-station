6e2f7b247db22ad727bea0448b7985b6788c87d7
Jon Faviell <jon.faviell@gmail.com>
2022-09-01 17:05:34 -0500

    move to numbers station module

---
+++ infra/modules/numbers-station/variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
+}
+
+variable "account_number" {
+  description = "AWS account number"
+  type        = string
+  default     = ""
+}
+
+variable "cron" {
+  description = "Cron expression"
+  type        = string
+  default     = ""
+}
+
+variable "twitter_consumer_key" {
+  description = "Twitter consumer key"
+  type        = string
+  default     = ""
+}
+
+variable "twitter_consumer_secret" {
+  description = "Twitter consumer secret"
+  type        = string
+  default     = ""
+}
+
+variable "twitter_access_token" {
+  description = "Twitter access token"
+  type        = string
+  default     = ""
+}
+
+variable "twitter_access_token_secret" {
+  description = "Twitter access token secret"
+  type        = string
+  default     = ""
+}
