ed967e193a4ea7943ba2664fa52ac38f23eb33c7
Jon Faviell <jon.faviell@gmail.com>
2022-09-02 14:57:37 -0500

    remove comments

--- infra/modules/function-secrets/outputs.tf
+++ infra/modules/function-secrets/outputs.tf
@@ -1,4 +1,3 @@
-# twitter consumer key
 output "twitter_consumer_key_arn" {
   value = aws_secretsmanager_secret.twitter_consumer_key.arn
 }
@@ -6,7 +5,6 @@ output "twitter_consumer_key_name" {
   value = aws_secretsmanager_secret.twitter_consumer_key.name
 }
 
-# twitter consumer secret
 output "twitter_consumer_secret_arn" {
   value = aws_secretsmanager_secret.twitter_consumer_secret.arn
 }
@@ -14,7 +12,6 @@ output "twitter_consumer_secret_name" {
   value = aws_secretsmanager_secret.twitter_consumer_secret.name
 }
 
-# twitter access token
 output "twitter_access_token_arn" {
   value = aws_secretsmanager_secret.twitter_access_token.arn
 }
@@ -22,7 +19,6 @@ output "twitter_access_token_name" {
   value = aws_secretsmanager_secret.twitter_access_token.name
 }
 
-# twitter access token secret
 output "twitter_access_token_secret_arn" {
   value = aws_secretsmanager_secret.twitter_access_token_secret.arn
 }
