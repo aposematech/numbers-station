9444ce6f45dcd079acb700029dfa1f231d2f5dbd
Jon Faviell <jon.faviell@gmail.com>
2022-09-02 12:01:50 -0500

    move to image repo module

--- infra/main.tf
+++ infra/main.tf
@@ -39,38 +39,8 @@ module "git_repo" {
   git_secret_aws_access_key         = var.git_secret_aws_access_key
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = terraform.workspace
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy
-resource "aws_ecr_lifecycle_policy" "ecr_repo_lifecycle_policy" {
-  repository = aws_ecr_repository.ecr_repo.name
-  policy     = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Expire untagged images older than 1 day",
-      "selection": {
-        "tagStatus": "untagged",
-        "countType": "sinceImagePushed",
-        "countUnit": "days",
-        "countNumber": 1
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-}
-EOF
+module "image_repo" {
+  source = "./modules/image-repo"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
---
+++ infra/modules/image-repo/main.tf
@@ -0,0 +1,33 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = terraform.workspace
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy
+resource "aws_ecr_lifecycle_policy" "ecr_repo_lifecycle_policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire untagged images older than 1 day",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
