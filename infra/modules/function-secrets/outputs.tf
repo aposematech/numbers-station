63214345b208fd8c7612ef9e9093f2c8daacc5b0
Jon Faviell <jon.faviell@gmail.com>
2022-09-02 17:51:59 -0500

    edit output desc and order

--- infra/modules/function-secrets/outputs.tf
+++ infra/modules/function-secrets/outputs.tf
@@ -1,27 +1,39 @@
-output "twitter_consumer_key_arn" {
-  value = aws_secretsmanager_secret.twitter_consumer_key.arn
-}
 output "twitter_consumer_key_name" {
-  value = aws_secretsmanager_secret.twitter_consumer_key.name
+  description = "Twitter Consumer Key - AWS Secret Name"
+  value       = aws_secretsmanager_secret.twitter_consumer_key.name
+}
+
+output "twitter_consumer_key_arn" {
+  description = "Twitter Consumer Key - AWS Secret ARN"
+  value       = aws_secretsmanager_secret.twitter_consumer_key.arn
+}
+
+output "twitter_consumer_secret_name" {
+  description = "Twitter Consumer Secret - AWS Secret Name"
+  value       = aws_secretsmanager_secret.twitter_consumer_secret.name
 }
 
 output "twitter_consumer_secret_arn" {
-  value = aws_secretsmanager_secret.twitter_consumer_secret.arn
+  description = "Twitter Consumer Secret - AWS Secret ARN"
+  value       = aws_secretsmanager_secret.twitter_consumer_secret.arn
 }
-output "twitter_consumer_secret_name" {
-  value = aws_secretsmanager_secret.twitter_consumer_secret.name
+
+output "twitter_access_token_name" {
+  description = "Twitter Access Token - AWS Secret Name"
+  value       = aws_secretsmanager_secret.twitter_access_token.name
 }
 
 output "twitter_access_token_arn" {
-  value = aws_secretsmanager_secret.twitter_access_token.arn
+  description = "Twitter Access Token - AWS Secret ARN"
+  value       = aws_secretsmanager_secret.twitter_access_token.arn
 }
-output "twitter_access_token_name" {
-  value = aws_secretsmanager_secret.twitter_access_token.name
+
+output "twitter_access_token_secret_name" {
+  description = "Twitter Access Token - AWS Secret Name"
+  value       = aws_secretsmanager_secret.twitter_access_token_secret.name
 }
 
 output "twitter_access_token_secret_arn" {
-  value = aws_secretsmanager_secret.twitter_access_token_secret.arn
-}
-output "twitter_access_token_secret_name" {
-  value = aws_secretsmanager_secret.twitter_access_token_secret.name
+  description = "Twitter Access Token - AWS Secret ARN"
+  value       = aws_secretsmanager_secret.twitter_access_token_secret.arn
 }
