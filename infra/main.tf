d97b3f49c22a1ef560f95bfd6c624ce79d4b8e33
Jon Faviell <jon.faviell@gmail.com>
2022-11-19 22:14:56 -0600

    add topics

--- infra/main.tf
+++ infra/main.tf
@@ -53,6 +53,7 @@ module "git" {
   git_repo_name           = terraform.workspace
   git_repo_description    = var.git_repo_description
   git_repo_homepage_url   = var.twitter_url
+  git_repo_topics         = ["bot", "demo", "twitter"]
   git_repo_visibility     = var.git_repo_visibility
   aws_access_key_id_name  = "AWS_ACCESS_KEY_ID"
   aws_access_key_id_value = var.aws_access_key_id
