fcca31635ee0b40c150ecffba9cc5bfeee20ae04
Jon Faviell <jon.faviell@gmail.com>
2022-11-22 13:52:06 -0600

    use tmp

--- app/app.py
+++ app/app.py
@@ -37,11 +37,12 @@ def handler(event, context):
     cipher_text = get_cipher_text_blocks(get_cipher_text(alphabet, plain_text, one_time_pad), 5)
     # create qrcode
     cipher_text_qr_code_filename = str(uuid.uuid4()) + ".png"
+    cipher_text_qr_code_filepath = "/tmp/" + cipher_text_qr_code_filename
     cipher_text_qr_code = qrcode.make(cipher_text)
-    cipher_text_qr_code.save(cipher_text_qr_code_filename)
+    cipher_text_qr_code.save(cipher_text_qr_code_filepath)
     # upload qrcode
     s3_client = boto3.client('s3')
-    with open(cipher_text_qr_code_filename, "rb") as f:
+    with open(cipher_text_qr_code_filepath, "rb") as f:
         s3_client.upload_fileobj(f, os.environ['WEBSITE_BUCKET_NAME'], os.environ['BUCKET_FOLDER_NAME'] + "/" + cipher_text_qr_code_filename)
     # ping heartbeat monitor
     requests.get(os.environ['HEARTBEAT_MONITOR_URL'])
