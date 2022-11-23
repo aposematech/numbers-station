fca88f47f6dcd8e28966604f010c5c51cab4c3e0
Jon Faviell <jon.faviell@gmail.com>
2022-11-22 20:23:52 -0600

    increase qrcode size

--- app/backend/app.py
+++ app/backend/app.py
@@ -41,7 +41,7 @@ def handler(event, context):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
+        box_size=67,
         border=4,
     )
     qr.add_data(cipher_text)
