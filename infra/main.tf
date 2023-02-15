1b75c64284700dd95503f624b1172e238b2180e1
Jon Faviell <jon.faviell@gmail.com>
2023-02-15 13:22:21 -0600

    use providers.tf file

--- infra/main.tf
+++ infra/main.tf
@@ -1,27 +1,3 @@
-# https://registry.terraform.io/providers/integrations/github/latest/docs
-provider "github" {
-  # export GITHUB_TOKEN
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-provider "aws" {
-  region = var.aws_region
-  # export AWS_ACCESS_KEY_ID
-  # export AWS_SECRET_ACCESS_KEY
-
-  default_tags {
-    tags = {
-      Terraform = "true"
-      Workspace = terraform.workspace
-    }
-  }
-}
-
-# https://registry.terraform.io/providers/BetterStackHQ/better-uptime/latest/docs
-provider "betteruptime" {
-  api_token = var.betteruptime_api_token
-}
-
 module "git" {
   source                  = "./modules/git"
   git_repo_name           = terraform.workspace
